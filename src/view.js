ed163116e323e41ce7d8b37288661e54ebe33561
PenLogos <penelopelogos@gmail.com>
2023-10-09 12:26:16 -0500

    maquetación sin estilos vista crear cuenta y botón para ingresar en ella

--- src/view.js
+++ src/view.js
@@ -1,45 +1,42 @@
 export const renderLogin = () => {
+  const container = document.createElement("div");
+  container.classList.add("login-page")
 
-  const container = document.createElement('div');
+  const logo = document.createElement("img");
+  logo.setAttribute("src", "./images/LogoinversoPeque.png");
+  logo.setAttribute("alt", "Logo mochileiros");
+  logo.setAttribute("class", "logo-invertido");
 
-  const logo = document.createElement('img');
-  logo.setAttribute('src', './images/LogoinversoPeque.png');
-  logo.setAttribute('alt', 'Logo mochileiros');
-  logo.setAttribute('class', 'logo-invertido');
+  const h1 = document.createElement("h1");
+  h1.textContent = "Mochileiros";
 
-  const h1 = document.createElement('h1');
-  h1.textContent = 'Mochileiros';
+  const labelUsername = document.createElement("label");
+  labelUsername.setAttribute("for", "user");
 
-  const labelUsername = document.createElement('label');
-  labelUsername.setAttribute('for', 'user');
+  const inputUsername = document.createElement("input");
+  inputUsername.setAttribute("type", "text");
+  inputUsername.setAttribute("placeholder", "Ingresa tu nombre de usuario");
+  inputUsername.classList.add("inputs");
 
-  const inputUsername = document.createElement('input');
-  inputUsername.setAttribute('type', 'text');
-  inputUsername.setAttribute('placeholder', 'Ingresa tu nombre de usuario');
-  inputUsername.classList.add('inputs');
+  const labelPassword = document.createElement("label");
+  labelPassword.setAttribute("for", "password");
 
-  const labelPassword = document.createElement('label');
-  labelPassword.setAttribute('for', 'password');
+  const inputPassword = document.createElement("input");
+  inputPassword.setAttribute("type", "password");
+  inputPassword.setAttribute("placeholder", "Ingresa tu contraseña");
+  inputPassword.classList.add("inputs");
 
-  const inputPassword = document.createElement('input');
-  inputPassword.setAttribute('type', 'password');
-  inputPassword.setAttribute('placeholder', 'Ingresa tu contraseña');
-  inputPassword.classList.add('inputs');
+  const btnLogin = document.createElement("button");
+  btnLogin.setAttribute("id", "login");
+  btnLogin.textContent = "Ingresar";
 
-  const btnLogin = document.createElement('button');
-  btnLogin.setAttribute('id', 'login');
-  btnLogin.textContent = 'Ingresar';
+  const btnCreateAccount = document.createElement("button");
+  btnCreateAccount.setAttribute("id", "account");
+  btnCreateAccount.textContent = "Crear cuenta";
 
-  const btnCreateAccount = document.createElement('button');
-  btnCreateAccount.setAttribute('id', 'account');
-  btnCreateAccount.textContent = 'Crear cuenta';
+  const btnGoogle = document.createElement("button");
+  btnGoogle.setAttribute("id", "google");
 
-  const btnGoogle = document.createElement('button');
-  btnGoogle.setAttribute('id', 'google');
-
-  // labelUsername.appendChild(inputUsername);
-  // labelPassword.appendChild(inputPassword);
-  
   container.appendChild(logo);
   container.appendChild(h1);
   container.appendChild(labelUsername);
@@ -51,4 +48,21 @@ export const renderLogin = () => {
   container.appendChild(btnGoogle);
 
   return container;
+};
+
+export const renderCreateAccount = () => {
+  const containerAccount = document.createElement("div");
+  containerAccount.classList.add('create-acount-page')
+  const form = `
+  <img src="./images/LogoPeque.png" alt="logo-mochileiros">
+  <h2>Crear cuenta</h2>
+  <input type="text" placeholder="Usuario">
+  <input type="email" placeholder="Correo electrónico">
+  <input type="password" placeholder="Contraseña">
+  <input type="password" placeholder="Confirma contraseña">
+  <button id="create-account">Crear</button>
+  <button id="google-create"></button>
+  `;
+  containerAccount.innerHTML = form
+  return containerAccount;
 };
