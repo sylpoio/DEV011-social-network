9d9382a4ce06bc72e953ff5ce196c392df10d970
danae <cdanae.rod@gmail.com>
2023-10-19 14:47:16 -0600

    Error de test resuelto

--- test/components/login-test.spec.js
+++ test/components/login-test.spec.js
@@ -6,7 +6,7 @@ import { renderLogin } from '../../src/components/login';
 // import { navigateTo } from '../../src/main'
 
 jest.mock('../../src/lib/auth', () => ({
-  loginFunctionMock: jest.fn(() => Promise.resolve()),
+  loginFunction: jest.fn(() => Promise.resolve()),
 }));
 
 describe('renderLogin', () => {
@@ -27,25 +27,24 @@ describe('renderLogin', () => {
     createAccountButton.click();
     expect(navigateToMock).toHaveBeenLastCalledWith('/signin');
   });
-/*   it('cuando se hace click en el botón crear cuenta nos lleva a la url de feed', () => {
+  it('cuando se hace click en el botón crear cuenta nos lleva a la url de feed', (done) => {
     const DOM = document.createElement('div');
     const navigateToMock = jest.fn();
-    const email = document.querySelector('#inputUsername');
-    const password = document.querySelector('#inputPassword');
-    const loginFunctionMock = jest.fn((email, password) => {
-      if(email === 'mochilerxs@gmail.com' && password === '123456') {
-        return Promise.resolve(navigateToMock)
-      } else {
-        return Promise.reject(new Error('error'))
-      }
-    });
     
-    DOM.append(renderLogin(loginFunctionMock));
-    DOM.append(loginFunctionMock(navigateToMock));
+        
+    DOM.append(renderLogin(navigateToMock));
+    const emailInput = DOM.querySelector('#inputUsername');
+    const passwordInput = DOM.querySelector('#inputPassword');
+    emailInput.value = 'mochilerxs@gmail.com';
+    passwordInput.value = '123456';
     const loginButton = DOM.querySelector('#login');
     loginButton.click();
-    expect(navigateToMock).toHaveBeenLastCalledWith('/feed');
-  }); */
+    
+    setTimeout(() => {
+      expect(navigateToMock).toHaveBeenLastCalledWith('/feed');
+      done()
+    });
+  });
 });
 
 
