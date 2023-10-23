c1f3920e64b71e48a62431d3a82e270b9f29c3ad
PenLogos <142206877+PenLogos@users.noreply.github.com>
2023-10-23 11:24:24 -0500

    docs afuera

--- docs/components/error.js
+++
@@ -1,6 +0,0 @@
-export const error = () => {
-  const title = document.createElement('h2');
-  title.textContent = 'Error 404 page no found, please go home';
-  title.classList.add('');
-  return title;
-};
