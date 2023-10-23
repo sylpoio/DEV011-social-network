3324983af0ee37d3e24404cd5e506f5b7c237a63
sylpoio <syl.gomez.bio@gmail.com>
2023-10-23 17:48:02 -0600

    Completo, falta catch

--- test/components/create-account-test.spec.js
+++ test/components/create-account-test.spec.js
@@ -1,32 +1,70 @@
-// /**
-//  * @jest-environment jsdom
-//  */
+/**
+* @jest-environment jsdom
+*/
 
-// import { createAccountFunction } from '../../src/lib/auth';
-// import { renderCreateAccount } from '../../src/components/create-account';
+import { renderCreateAccount } from '../../src/components/create-account';
+// import { navigateTo } from '../../src/main'
 
-// jest.mock('../src/lib/auth', () => ({
-//   createAccountFunction: jest.fn(() => Promise.resolve()),
-// }));
+jest.mock('../../src/lib/auth', () => ({
+  createAccountFunction: jest.fn(() => Promise.resolve()),
+  createAccountReject: jest.fn(() => Promise.reject()),
+  accountGoogle: jest.fn(() => Promise.resolve()),
+}));
 
-// describe('createAccountFunction', () => {
-//   it('debería ser una función', () => {
-//     expect(typeof createAccountFunction).toBe('function');
-//   });
-//   // it('la función crea una cuenta', () => {
-//   //   const user = 'mochilerxs@gmail.com';
-//   //  expect().toBe('Usuario creado con éxito:', user);
-//   // });
-// });
-
-// describe('renderCreateAccount', () => {
-//   it('tiene un botón para crear cuenta', () => {
-//     const DOM = document.createElement('main');
-//     const div = document.createElement('div');
-//     DOM.appendChild(div);
-//     DOM.setAttribute('id', 'root');
-//     DOM.append(renderCreateAccount);
-//     const haveCreateButton = DOM.querySelector('#signin');
-//     expect(haveCreateButton).toBe(undefined);
-//   });
-// });
+describe('renderCreateAccount', () => {
+  it('debería ser una función', () => {
+    expect(typeof renderCreateAccount).toBe('function');
+  });
+  it('tiene un botón para crear cuenta', () => {
+    const DOM = document.createElement('div');
+    DOM.append(renderCreateAccount());
+    const haveCreateButton = DOM.querySelector('#signin');
+    expect(haveCreateButton).not.toBe(undefined);
+  });
+  it('cuando se hace click en el botón crear nos lleva a la url de feed', (done) => {
+    const DOM = document.createElement('div');
+    const navigateToMock = jest.fn();
+    DOM.append(renderCreateAccount(navigateToMock));
+    const email = DOM.querySelector("input[type='email']");
+    const password = DOM.querySelector("input[type='password']");
+    const validatePassword = DOM.querySelector("input[placeholder='Confirma contraseña']");
+    email.value = 'mochilerxs@gmail.com';
+    password.value = '123456';
+    validatePassword.value = '123456';
+    const siginButton = DOM.querySelector('#signin');
+    siginButton.click();
+    setTimeout(() => {
+      expect(navigateToMock).toHaveBeenLastCalledWith('/feed');
+      done();
+    });
+  });
+  it('cuando se hace click en el botón crear y no cumple los requerimientos para login, muestra los errores correspondientes', () => {
+    const DOM = document.createElement('div');
+    const navigateToMock = jest.fn();
+    DOM.append(renderCreateAccount(navigateToMock));
+    const email = DOM.querySelector("input[type='email']");
+    const password = DOM.querySelector("input[type='password']");
+    const validatePassword = DOM.querySelector("input[placeholder='Confirma contraseña']");
+    const signinButton = DOM.querySelector('#signin');
+    signinButton.click();
+    const errorSpan = DOM.querySelector('.error-message');
+    if (email.value === 'mochilerxs'
+    && password.value === '' && validatePassword.value === '') {
+      expect(errorSpan).toBe('Correo invalido');
+    } else if (email.value === 'g@gmail.com'
+    && password.value === '12345' && validatePassword.value === '') {
+      expect(errorSpan).toBe('La contraseña requiere mínimo 6 caracteres');
+    }
+  });
+  it('cuando se hace click en el botón de google nos lleva a la url de feed', (done) => {
+    const DOM = document.createElement('div');
+    const navigateToMock = jest.fn();
+    DOM.append(renderCreateAccount(navigateToMock));
+    const googleButton = DOM.querySelector('#signin-google');
+    googleButton.click();
+    setTimeout(() => {
+      expect(navigateToMock).toHaveBeenLastCalledWith('/feed');
+      done();
+    });
+  });
+});
