f17d9bb9717876056ca95edff4e4afb7dbf4e5ad
Sylvia Gomez <syl.gomez.bio@gmail.com>
2023-10-26 11:50:48 -0600

    corrección error page

--- src/components/error.js
+++ src/components/error.js
@@ -1,6 +1,7 @@
 export const error = () => {
   const title = document.createElement('h2');
+  console.log(title);
   title.textContent = 'Error 404 page no found, please go home';
-  title.classList.add('');
+  title.classList.add('error');
   return title;
 };
